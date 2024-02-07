a8f3c6a08d8f5241325aced134e6088463103c97
BragancaFeup <braganca@fe.up.pt>
2024-02-07 19:14:11 +0000

    Teste Commit spyder

---
+++ Teste-Spyder.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 19:10:16 2024
+
+@author: braga
+"""
+
+Print( "teste com spyder")
---
+++ testeAddGit_old.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 15:57:27 2024
+
+@author: braga
+"""
+
+print("teste add Git")
+<<<<<<< HEAD
+
+
+print("teste alteracão e reenvio para Git")
+=======
+print("alterado por bo gmail")
+>>>>>>> e77f5de44418e783482fad053558a5e174589998
