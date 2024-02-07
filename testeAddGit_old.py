11f690b734b59ad26c2cff0c1a1532e6f5a02793
BragancaFeup <braganca@fe.up.pt>
2024-02-07 19:40:13 +0000

    teste de commit com spyder

--- testeAddGit_old.py
+++ testeAddGit_old.py
@@ -6,10 +6,7 @@ Created on Thu Feb  1 15:57:27 2024
 """
 
 print("teste add Git")
-<<<<<<< HEAD
-
-
 print("teste alteracão e reenvio para Git")
-=======
 print("alterado por bo gmail")
->>>>>>> e77f5de44418e783482fad053558a5e174589998
+
+print("alteração no splyder")
